ecfc900db0e19f6d9a8b1c1aa63e501129e85fd6
Hector Rincon <hecerinc@hotmail.com>
2017-11-18 01:03:01 -0800

    Add UI for data layer filters and file report button

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'normalize.css';
 
-// import registerServiceWorker from './registerServiceWorker';
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
+
+
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
 	<Router>
