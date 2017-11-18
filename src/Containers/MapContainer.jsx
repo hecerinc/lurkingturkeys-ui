77d4562337513d7909d6c9adbb8edb08da975651
Hector Rincon <hecerinc@hotmail.com>
2017-11-18 01:03:01 -0800

    Add UI for data layer filters and file report button

--- src/Containers/MapContainer.jsx
+++ src/Containers/MapContainer.jsx
@@ -1,17 +1,57 @@
 // MapContainer.js
 
 import React from 'react';
-import './Map.css';
+import '../App.css';
+import '../Map.css';
+import mapboxgl from 'mapbox-gl'
 
-const mapboxgl = process.env.MAPBOXAPIKEY;
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOXAPIKEY;
 
 export default class MapContainer extends React.Component {
-
+	constructor() {
+		super();
+		this.state = {
+			lat: 23.2199952,
+			lng: -102.1720662,
+			zoom: 6,
+			layers: ['Mines', 'Polluted water sources', 'Dams', 'Poverty', 'Indigenous communities', 'Landfills', 'PM2.5', 'Power plants'] // power plants includes refineries
+		};
+	}
+	componentDidMount() {
+		const { lng, lat, zoom } = this.state;
+		const map = new mapboxgl.Map({
+		    container: 'map',
+		    style: 'mapbox://styles/mapbox/streets-v10',
+		    center: [lng, lat],
+		    zoom
+		});
+	}
 	render() {
 		return (
-			<div>
-				<h1>Map Container</h1>
-			</div>
+			<section className="MapContainer">
+				<div className="container cf">
+					<div className="left">
+						<div id="map"></div>
+					</div>
+					<div className="sidebar">
+						<section>
+							<h2>Data layers</h2>
+							<div className="row layers">
+								{this.state.layers.map(layer => (
+									<label>
+										<input type="checkbox" />
+										{layer}
+									</label>
+								))}
+							</div>
+							<hr/>
+						</section>
+						<section>
+							<a href="#" className="btn" id="newreport">+ File report</a>
+						</section>
+					</div>
+				</div>
+			</section>
 		)
 	}
 }
